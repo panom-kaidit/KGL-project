5557529133dfc88794fb49255f24ffc280aac81e
Panom M <p.makuei@alustudent.com>
2026-01-16 18:33:31 +0300

    anotherday  we continue

---
+++ loginform/JS/loginscript.js
@@ -0,0 +1,6 @@
+const { constant } = require("lodash");
+
+const Signinform = document.getElementById("form-box");
+
+const userMail = document.getElementById("email");
+const passCode = document.getElementById("passcode")
