f8be9a41bd67f0454fa43b2c6bfd2e221edb9782
Panom M <p.makuei@alustudent.com>
2026-02-15 22:47:07 +0300

    :)

--- KGLBackend/src/models/User.js
+++ KGLBackend/src/models/User.js
@@ -1,22 +1,23 @@
-const mongoose = require("mongoose");
-
-const userSchema= new mongoose.Schema({
-    name: {
-        type:String,
-        required
-    },
-    email:{
-        type: String,
-        unique: true,
-        required
-    },
-    password: {
-        type: String,
-        required
-    },
-    role:{
-        type: String
-    }
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ["manager", "sales agent", "director"],
+    default: "sales agent"
+  }
 });
-
-module.exports = mongoose.model("User", userSchema)
